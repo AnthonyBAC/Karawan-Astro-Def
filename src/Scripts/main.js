3fc675e0ce83c6013af0f092364132e005695fdb
Anthony Adasme <anthony.adasme@gmail.com>
2023-06-09 21:42:12 -0400

    karawan

--- src/Scripts/main.js
+++ src/Scripts/main.js
@@ -1,20 +1,51 @@
+//appear
+const appear = document.querySelector('.appear'); 
+const cb = function(entries){
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('inview');
+    }else{
+      entry.target.classList.remove('inview');
+    }
+  });
+}
+const io = new IntersectionObserver(cb);
+io.observe(appear);
 
 
-    const fadeElements = document.querySelectorAll(".fade-in");
 
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("fadeInUp");
-                observer.unobserve(entry.target);
-            }
-        });
+
+const items = document.querySelectorAll('.appear2');
+
+const active = function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+        entry.target.classList.add('inview2'); 
+        }else{
+            entry.target.classList.remove('inview2'); 
+        }
     });
+}
+const io2 = new IntersectionObserver(active);
+ for(let i=0; i < items.length; i++){
+    io2.observe(items[i]);
+ }
 
-    fadeElements.forEach((element) => {
-        observer.observe(element);
-    });
 
+const appear3 = document.querySelector('.appear3'); 
+const cb3 = function(entries){
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('inview');
+      entry.unobserve(entry.target)
+    }
+  });
+}
+const io3 = new IntersectionObserver(active);
+io3.observe(appear3);
+///appear
+
+    
 
 ///scroll top
     window.onbeforeunload = function () {
